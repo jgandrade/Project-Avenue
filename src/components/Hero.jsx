aba80235122ca3b1df197f9d7de148e7a23d67c3
jgandrade1 <johnglenn.andrade@gmail.com>
2022-12-09 21:51:20 +0800

    Update Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,14 +9,18 @@ function Hero() {
         className="absolute left-[-20%] top-0 sm:top-[-30%] z-1"
       />
       <img src="./orange-ball-bg.png" className="absolute top-0 right-0 z-1" />
-      <p className="italic font-semibold w-[80vw] sm:w-100vw text-[clamp(18px,2vw,27px)] mb-3 z-10">
+      <p className="italic font-semibold w-[80vw] sm:w-100vw text-[clamp(18px,2vw,18px)] mb-3 z-10">
         They're talking about you, Don't you wanna talk back to them?
       </p>
-      <h2 className="font-bold text-[clamp(32px,5vw,3em)] w-[70vw] sm:w-[40vw] text-center z-10 relative ">
-        Every brand has a{" "}
-        <span className="underline decoration-[#FFD900]">story.</span> Let us
-        help tell yours.
-      </h2>
+      <div className="relative">
+        <h2 className="font-bold text-[clamp(32px,5vw,3em)] sm:w-[40vw] text-center z-10">
+          Every brand has a{" "}
+          <span className="underline decoration-[#FFD900]">story.</span>
+        </h2>
+        <h2 className="font-bold text-[clamp(32px,5vw,3em)] sm:w-[40vw] text-center z-10">
+          Let us help tell yours.
+        </h2>
+      </div>
       <p className="italic font-semibold w-[60vw] sm:w-[35vw] text-[clamp(16px,1vw,24px)] text-center mt-10 mb-14 z-10">
         A personalized platform specifically built for start-up to help you to
         support your 🚀 growth & expand your 🏘️ neighborhood.
